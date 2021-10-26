93c2eb4f18cd5e1a6a69d1637a1db4e2a96cdf24
Om_jannu <omjannu2002@gmail.com>
2021-10-26 21:20:23 +0530

    updated lines

--- src/assign2/a2q2.java
+++ src/assign2/a2q2.java
@@ -2,6 +2,6 @@ package assign2;
 //Write a Java program to illustrate Constructor Chaining
 public class a2q2{
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hellort ");
     }
 }
