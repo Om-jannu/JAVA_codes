ea6cb688012480785d8603bdc23a1cf8be97e2bb
Om_jannu <omjannu2002@gmail.com>
2021-10-30 20:38:29 +0530

    feat: constructor chaining

--- src/assign2/a2q2.java
+++ src/assign2/a2q2.java
@@ -1,7 +1,39 @@
 package assign2;
 //Write a Java program to illustrate Constructor Chaining
-public class a2q2{
-    public static void main(String[] args) {
-        System.out.println("hellort ");
+      class usingthis {
+    //constructor chaining using this()
+
+    //default constructor
+    usingthis() {
+        //constructor 1
+        this(20);//constructor 3 is called
+        System.out.println("default constructor called");
+    }
+
+    usingthis(int age) {
+        //constructor 2
+        System.out.println("parameterized constructor - int as parameter");
     }
 }
+   class usingsuper extends usingthis{
+         //constructor chaining using super()
+
+         //derived default constructor1
+       usingsuper(){
+           this("name","java"); //derived default constructor2 is called
+           System.out.println("derived class default constructor called");
+       }
+
+       //derived default constructor2
+       usingsuper(String name,String java){
+           super();//parent class - usingthis is called
+           System.out.println("derived class parameterized constructor called");
+       }
+     }
+     public class a2q2{
+         public static void main(String[] args) {
+             usingsuper us = new usingsuper();
+             System.out.println("------------------------------------------------------------------");
+             usingthis ut = new usingthis();
+         }
+     }
